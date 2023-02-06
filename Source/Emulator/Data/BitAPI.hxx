93c5cdae1253d89d3c4cf78823ea783fcec57ccd
xlang <xlangk@proton.me>
2023-02-05 19:48:43 -0500

    Feat: Added `BitAPI.hxx` class for collecting bits and connecting `dev`

---
+++ Source/Emulator/Data/BitAPI.hxx
@@ -0,0 +1,86 @@
+#pragma once
+#include <iostream>
+#include <exception>
+
+namespace emulator {
+class BitAPI {
+private:
+    bool *m_packet;
+    bool m_autoTrigger;
+    bool m_detectCorruption = true;
+    size_t m_packetSize;
+    size_t m_newestBits{};
+
+    void reset(bool reBuildPacket = true) {
+        delete m_packet;
+        m_newestBits = 0;
+        if (reBuildPacket)
+            m_packet = new bool[m_packetSize];
+    }
+
+public:
+    explicit BitAPI(bool autoTrigger = false, size_t packetSize = 1) : m_autoTrigger(autoTrigger), m_packetSize(packetSize), m_newestBits(0) {
+        m_packet = new bool[packetSize];
+    }
+
+    ~BitAPI() {
+        reset(false);
+    }
+
+    void funnelBit(bool bit) {
+        m_packet[m_newestBits] = bit;
+        m_newestBits++;
+
+        collectDirectDrive(bit);
+
+        if (m_autoTrigger && m_newestBits == m_packetSize) {
+            collectPacket(m_packet, m_packetSize);
+            reset();
+        }
+    }
+
+    void setTriggerMode(bool automatic = true) {
+        m_autoTrigger = automatic;
+    }
+
+    void setPacketSize(size_t size) {
+        if (size > 0) m_packetSize = size;
+    }
+
+    void setDetectCorruption(bool detect = true) {
+        m_detectCorruption = detect;
+    }
+
+    void flush() {
+        if (m_detectCorruption && m_newestBits > m_packetSize)
+            throw std::runtime_error("Corrupted packet detected! (Packet size: " + std::to_string(m_packetSize) + ", Newest bits: " + std::to_string(m_newestBits) + ")");
+        if (m_packetSize > 0 && m_newestBits < m_packetSize)
+            for (size_t i = m_newestBits; i < m_packetSize; i++) m_packet[i] = false;
+
+        collectPacket(m_packet, m_packetSize);
+    }
+
+    [[nodiscard]] constexpr bool getTriggerMode() const {
+        return m_autoTrigger;
+    }
+
+    [[nodiscard]] constexpr size_t getPacketSize() const {
+        return m_packetSize;
+    }
+
+    [[nodiscard]] constexpr bool *getPacket() const {
+        return m_packet;
+    }
+
+    [[nodiscard]] constexpr size_t getNewestBits() const {
+        return m_newestBits;
+    }
+
+    [[nodiscard]] constexpr bool getDetectCorruption() const {
+        return m_detectCorruption;
+    }
+
+    virtual void collectDirectDrive(bool value) = 0;
+    virtual void collectPacket(bool *value, size_t size) = 0;
+};
+}
--- Source/Line/emulator.exe.cxx
+++ Source/Line/emulator.exe.cxx
@@ -1,7 +1,32 @@
 #include <iostream>
+#include "../Emulator/Data/BitAPI.hxx"
+
+class BitReader : public emulator::BitAPI {
+public:
+    BitReader() {
+        setTriggerMode(false);
+        setPacketSize(1);
+    }
+
+    void collectPacket(bool *packet, size_t size) override {
+        for (size_t i = 0; i < size; i++) {
+            std::cout << packet[i];
+        }
+        std::cout << std::endl;
+    }
+
+    void collectDirectDrive(bool bit) override {
+
+    }
+};
 
 int main(const int argc, const char* argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
+    BitReader reader;
+
+    reader.funnelBit(true);
+
+    reader.flush();
+
     return 0;
 }
